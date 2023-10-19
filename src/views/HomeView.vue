ce0a85867456469e888f8ce5459a8c258f769bcb
yarchan <86291116+yarchan@users.noreply.github.com>
2023-10-19 19:30:35 +0300

    -fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -58,7 +58,7 @@ export default {
     const redirectUri = 'https://jolly-cendol-1d3a96.netlify.app';
     const AppID = '	51773806'; 
     const v = '5.154'
-    const vkAuthUrl = `https://oauth.vk.com/authorize?client_id=51773846&display=page&redirect_uri=http://172.18.22.51:8080/list-friends&scope=friends&response_type=token&v=5.131&state=123456`;
+    const vkAuthUrl = `https://oauth.vk.com/authorize?client_id=51773846&display=page&redirect_uri=https://jolly-cendol-1d3a96.netlify.app/list-friends&scope=friends&response_type=token&v=5.131&state=123456`;
     return{
       v,
       AppID,
